364a3b244f066a152acd9b33e2ff09b1613e910f
Mic <micgh1031@gmail.com>
2015-12-09 14:45:27 -0800

    for deploy

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require( 'path' );
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(path.join(__dirname, "dist")));
+app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
 
+app.get('/', function (req, res) {
+  res.render('index', {});
+});
 app.listen(port);
