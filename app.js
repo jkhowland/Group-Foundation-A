0a3c7faf05422e9aa591989052db82f5d3281b93
Mic <micgh1031@gmail.com>
2015-12-09 14:16:10 -0800

    fixing for deploy

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + "/dist"));
+app.get('/', function(req, res){
+  res.render('index.html');
+});
 app.listen(port);
