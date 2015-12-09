9f34ece9cd988bdfbb4ddd944e551512fb7bbfca
Mic <micgh1031@gmail.com>
2015-12-09 14:41:57 -0800

    for deploy

--- app.js
+++ app.js
@@ -3,7 +3,5 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + "/dist"));
-app.get('/', function(req, res){
-  res.sendfile(__dirname + "/index.html");
-});
+
 app.listen(port);
