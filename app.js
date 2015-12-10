39608ebda89ccde25f7b07e2499a3341fed562d8
Mic <micgh1031@gmail.com>
2015-12-09 16:45:01 -0800

    for deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, "/")));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "/") + 'index.html');
+  res.render('index.html');
 });
 
 app.listen(port);
