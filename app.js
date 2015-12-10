794ed44e7a5c344d1cb398963cc899a8b390c9b0
Mic <micgh1031@gmail.com>
2015-12-09 23:47:59 -0800

    for deploy

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var path = require( 'path' );
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 var app = express();
 
-app.use(express.static(path.join(__dirname, "/")));
-app.set('views', __dirname + '/');
+app.use(express.static(path.join(__dirname, "/app")));
+app.set('views', __dirname + '/app');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.get('/', function (req, res) {
