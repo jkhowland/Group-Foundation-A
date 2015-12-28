0295ba483728afc29e7676efd651152ca8737c08
Mic <micgh1031@gmail.com>
2015-12-28 08:24:27 -0500

    group listing

--- app/js/controllers/UserGroupController.js
+++ app/js/controllers/UserGroupController.js
@@ -1,18 +1,24 @@
 /* Setup general page controller */
-angular.module('foundationApp').controller('UserGroupController', ['$rootScope', '$scope', 'settings', 'UserGroup', function($rootScope, $scope, settings, UserGroup) {
-    $scope.$on('$viewContentLoaded', function() {
-        // initialize core components
-        App.initAjax();
+angular.module('foundationApp').controller('UserGroupController', ['$rootScope', '$scope', 'settings', 'UserGroup',
+    function($rootScope, $scope, settings, UserGroup) {
+        $scope.groups = [];
 
-        Layout.setSidebarMenuActiveLink('set', $('#sidebar_menu_link_groups'));
+        UserGroup.groups().then(function(groups){
+            $scope.groups = groups;
+        });
+        $scope.$on('$viewContentLoaded', function() {
+            // initialize core components
+            App.initAjax();
 
-        // set default layout mode
-        $rootScope.settings.layout.pageContentWhite = true;
-        $rootScope.settings.layout.pageBodySolid = false;
-        $rootScope.settings.layout.pageSidebarClosed = false;
-    });
+            Layout.setSidebarMenuActiveLink('set', $('#sidebar_menu_link_groups'));
 
-    $scope.init = function() {
+            // set default layout mode
+            $rootScope.settings.layout.pageContentWhite = true;
+            $rootScope.settings.layout.pageBodySolid = false;
+            $rootScope.settings.layout.pageSidebarClosed = false;
+        });
 
-    };
-}]);
+        $scope.init = function() {
+
+        };
+    }]);
